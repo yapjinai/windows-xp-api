631cbf747b90e0417631ef2b836312db242378c2
yapjinai <jinai.jyap@gmail.com>
2018-11-10 10:00:35 -0500

    got postgres set up

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_22_211701) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.string "name"
     t.string "content"
